7ba73dce2b6e089ef12b33607d3d489a42f80993
puneetsharma0910 <puneetsharma0910@gamil.com>
2024-10-28 17:53:28 +0530

    adding loop folder

---
+++ 1.0-Loops/nameprint.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i =0;
+    while(i<50){
+        cout<<"Puneet Sharma"<<" ";
+        i=i+1;
+    }
+return 0;
+}
---
+++ 1.0-Loops/table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 1; i < 11; i++)
+    {
+        /* code */
+        cout<<(7*i)<<endl;
+    }
+    
+return 0;
+}
